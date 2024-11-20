205c3e9458c9eac0f1ee102a56fec88f42edcd25
Low Tian Wei <42953280+lowtianwei@users.noreply.github.com>
2024-11-20 12:33:59 +0800

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-curl --location 'https://www.mewatch.sg/api/account/items/97104/videos?delivery=stream&ff=idp%2Cldp%2Crpt%2Ccd&lang=en&resolution=External&segments=all&sub=Registered' \
---header 'X-Authorization: {token}'
+curl --location 'https://cdn.mewatch.sg/api/items/97104/videos?delivery=stream%2Cprogressive&ff=idp%2Cldp%2Crpt%2Ccd&lang=en&resolution=External&segments=all'
