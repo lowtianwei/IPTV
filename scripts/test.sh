5ae791359f07cbd3c0afbf568469817f1af190e7
Low Tian Wei <42953280+lowtianwei@users.noreply.github.com>
2024-11-20 12:51:27 +0800

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-curl --location 'https://cdn.mewatch.sg/api/items/97104/videos?delivery=stream%2Cprogressive&ff=idp%2Cldp%2Crpt%2Ccd&lang=en&resolution=External&segments=all'
+resp=$(curl --location 'https://cdn.mewatch.sg/api/items/97104/videos?delivery=stream%2Cprogressive&ff=idp%2Cldp%2Crpt%2Ccd&lang=en&resolution=External&segments=all' | jq '.[2]')
+url=$(echo $resp | jq -r '.url')
+drm=$(echo $resp | jq -r '.drm')
+sed -i 's/{$url}/$url/g' template/widevine.template
+sed -i 's/{$drm}/$drm/g' template/widevine.template
+cat template/widevine.template
